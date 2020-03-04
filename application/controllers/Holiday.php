20044381f647134a9458aac7e627ef083d417897
Tuti Amalia <tutiamalia111@gmail.com>
2020-03-04 10:34:41 +0800

    perbaikan controller holiday

--- application/controllers/Holiday.php
+++ application/controllers/Holiday.php
@@ -7,6 +7,7 @@ class Holiday extends Admin_Controller
 	{
 		parent::__construct();
 		$this->load->model('m_holiday', 'holiday');
+		$this->load->library('form_validation');
 	}
 
 	public function index()
@@ -17,24 +18,18 @@ class Holiday extends Admin_Controller
         $this->load->view('admin/index', $data);
 	}
 
-	public function process(){
-		$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server
-		$db = mysql_select_db("hari_libur", $connection); // Selecting Database from Server
-		if(isset($_POST['simpan'])){ // Fetching variables of the form which travels in URL
-		$id_periode = $this->input->post['id_periode'];
-		$tanggal = $this->input-post['tanggal'];
-		$ket = $this->input->post['ket'];
-		if($tanggal !=''||$ket !=''){
-		//Insert Query of SQL
-		$query = mysql_query("insert into hari_libur(student_name, student_email, student_contact, student_address) values ('$name', '$email', '$contact', '$address')");
-		echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+	public function add(){
+		$holiday= $this->holiday;
+		$validation= $this->form_validation;
+		$validation->set_rules($holiday->rules());
+
+		if ($validation->run())
+		{
+			$holiday->save();
+			$this->session->set_flashdata('success','Berhasil disimpan');
 		}
-		else{
-		echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
-		}
-		}
-		mysql_close($connection); // Closing Connection with Server
-		?>
+
+		$this->load->view(admin/pages/holiday/form);
 	}
     
 }
