1871c20d5faa8f49432eb40df270443af6d465b4
Diki Siswanto <dickyachmady@gmail.com>
2020-03-12 10:37:15 +0800

    bug-fix: perbaiki bar chart di laman detail

--- assets/own/script.js
+++ assets/own/script.js
@@ -1,4 +1,4 @@
-$( function(){
+$(function () {
 	$('#calendar').datetimepicker({
 		format: 'L',
 		inline: true,
@@ -11,76 +11,84 @@ $( function(){
 		const rekapBulananCanvas = $('#rekap-bulanan').get(0).getContext('2d');
 		const data = {
 			labels: LABEL_BULANAN,
-			datasets: [
-				{
-					data: DATA_BULANAN,
-					backgroundColor: WARNA_BULANAN
-				}
-			]
+			datasets: [{
+				data: DATA_BULANAN,
+				backgroundColor: WARNA_BULANAN
+			}]
 		};
 		const options = {
-      maintainAspectRatio : false,
-      responsive : true,
+			maintainAspectRatio: false,
+			responsive: true,
 		}
 		const rekapBulanan = new Chart(rekapBulananCanvas, {
-      type: 'doughnut',
-      data: data,
-      options: options      
-    });
+			type: 'doughnut',
+			data: data,
+			options: options
+		});
 	}
 	if ($('#rekap-harian').length) {
 		const rekapHarianCanvas = $('#rekap-harian').get(0).getContext('2d');
+		const data = TANGGAL_HARIAN.map((tanggal, index) => ({
+			x: tanggal,
+			y: moment(`2020-02-01 ${JAM_HARIAN[index]}`).valueOf()
+		}));
 		const dataHarian = {
 			labels: TANGGAL_HARIAN,
-			datasets: [
-				{
-					data: JAM_HARIAN,
-					label: 'Jam Masuk',
-					backgroundColor : 'rgba(60,141,188,0.9)',
-          borderColor : 'rgba(60,141,188,0.8)',
-          pointRadius : false,
-          pointColor : '#3b8bba',
-          pointStrokeColor : 'rgba(60,141,188,1)',
-          pointHighlightFill : '#fff',
-          pointHighlightStroke : 'rgba(60,141,188,1)',
-				}
-			]
+			datasets: [{
+				data: data,
+				label: 'Jam Masuk',
+				backgroundColor: 'rgba(60,141,188,0.9)',
+				borderColor: 'rgba(60,141,188,0.8)',
+				pointRadius: false,
+				pointColor: '#3b8bba',
+				pointStrokeColor: 'rgba(60,141,188,1)',
+				pointHighlightFill: '#fff',
+				pointHighlightStroke: 'rgba(60,141,188,1)',
+			}]
 		};
 		const barOptions = {
-      maintainAspectRatio : false,
-			responsive : true,
+			maintainAspectRatio: false,
+			responsive: true,
 			datasetFill: false,
 			scales: {
-				yAxes: [
-					{
-						type: 'linear',
-						position: 'left',
-						ticks: {
-							min: moment('2020-02-01 00:00:00').valueOf(),
-							max: moment('2020-02-01 23:59:59').valueOf(),
-							stepSize: 3.6e+6,
-							beginAtZero: false,
-							callback: value => {
-								let date = moment(value);
-								if(date.diff(moment('2020-02-01 23:59:59'), 'minutes') === 0) {
-									return null;
-								}
-								
-								return date.format('HH:mm');
+				yAxes: [{
+					type: 'linear',
+					position: 'left',
+					ticks: {
+						min: moment('2020-02-01 05:00:00').valueOf(),
+						max: moment('2020-02-01 17:59:59').valueOf(),
+						stepSize: 3.6e+6,
+						beginAtZero: false,
+						callback: value => {
+							let date = moment(value);
+							if (date.diff(moment('2020-02-01 18:00:00'), 'minute') === 0) {
+								return null;
 							}
+							return date.format('HH:mm:ss');
 						}
 					}
-				]
+				}]
+			},
+			tooltips: {
+				callbacks: {
+					label: function (tooltipItem, data) {
+						let date = moment(tooltipItem.yLabel);
+						if (date.diff(moment('2020-02-01 18:00:00'), 'minute') === 0) {
+							return null;
+						}
+						return date.format('HH:mm:ss');
+					}
+				}
 			}
 		};
 		const rekapHarian = new Chart(rekapHarianCanvas, {
-      type: 'bar',
-      data: dataHarian,
-      options: barOptions      
-    });
+			type: 'bar',
+			data: dataHarian,
+			options: barOptions
+		});
 	}
 });
-$('.delete-btn').click(function(event) {
+$('.delete-btn').click(function (event) {
 	event.preventDefault();
 	const url = $(this).attr('href');
 	Swal.fire({
@@ -90,9 +98,9 @@ $('.delete-btn').click(function(event) {
 		showCancelButton: true,
 		confirmButtonText: 'Hapus',
 		cancelButtonText: 'Batalkan'
-	}).then(function(result) {
+	}).then(function (result) {
 		if (result.value) {
-			setTimeout(function() {
+			setTimeout(function () {
 				window.location = url;
 			}, 1000);
 			Swal.fire(
@@ -108,4 +116,3 @@ $('#datatable').DataTable({
 	ordering: false,
 	responsive: true
 });
-
