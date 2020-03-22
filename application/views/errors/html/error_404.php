edd2b85ec926fa9173679ac4c7f1327ef6bbf427
Diki Siswanto <dickyachmady@gmail.com>
2020-03-22 12:25:08 +0800

    update: modifikasi halaman error 404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,81 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= $heading ?></title>
+	<link rel="stylesheet" href="<?= base_url('assets/plugins/bootstrap/css/bootstrap.min.css') ?>">
+	<style>
+	/*======================
+    404 page
+=======================*/
+	.page_404 {
+		padding: 40px 0;
+		background: #fff;
+		font-family: 'Montserrat', serif;
+	}
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+	.page_404 img {
+		width: 100%;
+	}
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+	.four_zero_four_bg {
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+		background-image: url("<?= base_url('assets/own/404.gif') ?>");
+		height: 400px;
+		background-position: center;
+	}
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+	.four_zero_four_bg h1 {
+		font-size: 80px;
+	}
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+	.four_zero_four_bg h3 {
+		font-size: 80px;
+	}
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
+	.link_404 {
+		color: #fff !important;
+		padding: 10px 20px;
+		background: #39ac31;
+		margin: 20px 0;
+		display: inline-block;
+	}
 
-p {
-	margin: 12px 15px 12px 15px;
-}
+	.contant_box_404 {
+		margin-top: -50px;
+	}
+
 </style>
 </head>
+
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+	<section class="page_404">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 justify-content-center align-items-center text-center">
+					<div class="col-sm-10 mx-auto">
+						<div class="four_zero_four_bg">
+							<h1 class="font-weight-bold">404</h1>
+						</div>
+
+						<div class="contant_box_404">
+							<h3 class="h2 font-weight-bold">
+								Look like you're lost
+							</h3>
+							<p>the page you are looking for not avaible!</p>
+							<a href="<?= site_url() ?>" class="link_404">Go to Home</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </body>
+
 </html>
